47d2616a0184d9254aa62cb3237532e16d11d286
williamschwindt <33535511+williamschwindt@users.noreply.github.com>
2019-10-12 14:25:44 -0700

    Website Complete

---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+  
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;' 
+    });
+    
+    $('.js--scroll-to-plans').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });$('.js--scroll-to-start').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+    });
+    
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: "50%"
+    })
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: "50%"
+    })
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: "50%"
+    })
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: "50%"
+    })
+    
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200)
+        
+        if (icon.hasClass('ion-ios-menu')) {
+            icon.addClass('ion-ios-close');
+            icon.removeClass('ion-ios-menu');
+        } else {
+            icon.removeClass('ion-ios-close');
+            icon.addClass('ion-ios-menu');
+        }
+    })
+    
+});
+
+                                        
